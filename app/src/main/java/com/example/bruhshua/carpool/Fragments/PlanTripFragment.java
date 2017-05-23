5f4d61d7caf72d000c0b815b4cccb13d39c3210c
Joshua Cantero <cantero.joshua@gmail.com>
2017-05-23 15:50:29 -0700

    Integrated google maps API.

--- app/src/main/java/com/example/bruhshua/carpool/Fragments/PlanTripFragment.java
+++ app/src/main/java/com/example/bruhshua/carpool/Fragments/PlanTripFragment.java
@@ -3,17 +3,27 @@ package com.example.bruhshua.carpool.Fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.bruhshua.carpool.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
 /**
  * Created by bruhshua on 5/21/17.
  */
 
-public class PlanTripFragment extends Fragment {
+public class PlanTripFragment extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap map;
+    private SupportMapFragment mSupportMapFragment;
+
+
 
     public static PlanTripFragment newInstance(){
         PlanTripFragment planTripFragment = new PlanTripFragment();
@@ -24,12 +34,32 @@ public class PlanTripFragment extends Fragment {
     }
 
 
+
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.plan_trip_fragment,container,false);
 
+        mSupportMapFragment = SupportMapFragment.newInstance();
+        FragmentManager fm = getFragmentManager();
+        mSupportMapFragment.getMapAsync(this);
+        if(!mSupportMapFragment.isAdded())
+            fm.beginTransaction().add(R.id.map_fragment,mSupportMapFragment).commit();
+
+        else if(mSupportMapFragment.isAdded())
+            fm.beginTransaction().hide(mSupportMapFragment).commit();
+        else
+            fm.beginTransaction().show(mSupportMapFragment).commit();
+
 
         return v;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Log.d("PlantripFragment","onMapReady fired");
+        map = googleMap;
     }
 }
