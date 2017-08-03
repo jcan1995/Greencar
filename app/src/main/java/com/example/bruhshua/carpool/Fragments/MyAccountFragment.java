36903d6a881dabeb8eb7f723f7cdfac496488ee5
Joshua Cantero <cantero.joshua@gmail.com>
2017-08-03 15:44:34 -0700

    My Account UI updates.

--- app/src/main/java/com/example/bruhshua/carpool/Fragments/MyAccountFragment.java
+++ app/src/main/java/com/example/bruhshua/carpool/Fragments/MyAccountFragment.java
@@ -6,7 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.bruhshua.carpool.R;
 import com.example.bruhshua.carpool.Model.User;
 
@@ -22,6 +26,11 @@ public class MyAccountFragment extends Fragment {
     final private static String USER_TAG = "USER";
 
     private User authUser;
+    private TextView tvUserName;
+    private TextView tvPoints;
+    private TextView tvEmail;
+    private ImageView ivUserPhoto;
+    private ImageView ivBackGroundImage;
 
     public static MyAccountFragment newInstance(User user){
         MyAccountFragment myAccountFragment = new MyAccountFragment();
@@ -42,7 +51,27 @@ public class MyAccountFragment extends Fragment {
 
         View v = inflater.inflate(R.layout.my_account_fragment,container,false);
 
-        //Todo: Do whatever you want with the user's data.
+        if(authUser != null){
+
+            ivUserPhoto = (ImageView) v.findViewById(R.id.ivUserImage);
+            ivBackGroundImage = (ImageView) v.findViewById(R.id.ivBackGroundImage);
+            tvUserName = (TextView) v.findViewById(R.id.tvUserName);
+            tvPoints = (TextView) v.findViewById(R.id.tvPoints);
+            // tvEmail = (TextView) v.findViewById(R.id.tvEmail);
+
+            Glide.with(getActivity())
+                    .load(authUser.getDownloadUrl())
+                    .into(ivUserPhoto);
+            Glide.with(getActivity())
+                    .load(authUser.getDownloadUrl())
+                    .into(ivBackGroundImage);
+            ivBackGroundImage.setAlpha(.5f);
+           // ivBackGroundImage.
+            tvUserName.setText(authUser.getUserName());
+            tvPoints.setText(" | " + String.valueOf(authUser.getPoints()));
+           // tvEmail.setText(authUser.getEmail());
+
+        }
 
 
         return v;
